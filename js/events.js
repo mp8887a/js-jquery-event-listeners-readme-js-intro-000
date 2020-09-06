fb6efd0d788e9d0a040d4b38bba43cd7fe7f9a58
Mike Pokalsky <mpoksky1989@gmail.com>
2020-09-06 16:51:28 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -19,6 +19,12 @@ function pressIt(){
   });
 }
 
+function submitIt(){
+  $("form").on("submit", function(){
+    window.alert("Your form is going to be submitted now.");
+  });
+}
+
 
 $(document).ready(function(){
 
@@ -27,4 +33,5 @@ $(document).ready(function(){
 getIt();
 frameIt();
 pressIt();
+submitIt();
 });
