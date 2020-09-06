12f754a3a58ce56f762811961123b94f8e15a1d2
Mike Pokalsky <mpoksky1989@gmail.com>
2020-09-06 16:44:09 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,11 +1,13 @@
 //define functions here
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!");
+    });
+  }
+
 
 $(document).ready(function(){
-  function getIt(){
-    $("p").on("click", function(){
-      alert("Hey!");
-      });
-    }
+
 
 // call functions here
 getIt();
